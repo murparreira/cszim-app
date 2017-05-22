8a07da5067ad2fc893722c244077c901f57ac0ee
Murillo Parreira <murillo.parreira@agrodefesa.go.gov.br>
2017-05-22 09:22:35 -0300

    Gem do S3 e PG para o heroku

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,4 +1,5 @@
 require 'dragonfly'
+require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
@@ -8,9 +9,10 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  datastore :s3,
+    bucket_name: ENV["S3_BUCKET_NAME"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 end
 
 # Logger
