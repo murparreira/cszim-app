0c028ce713e16c906cdce61754909925afbf6a32
danilo.moraes <danilo.moraes@agrodefesa.go.gov.br>
2017-05-26 10:22:24 -0300

    Correção do random para não resetar automaticamente quando chegar no ultimo mapa

--- app/controllers/randomizer_controller.rb
+++ app/controllers/randomizer_controller.rb
@@ -7,13 +7,13 @@ class RandomizerController < ApplicationController
     if session[:maps].nil?
       init_maps
     end
-    if params[:random]
+    if params[:random] && session[:maps].any?
       session[:chosen_map] = session[:maps].sample
       session[:maps] = session[:maps] - [session[:chosen_map]]
       session[:chosen_maps] = [] if session[:chosen_maps].nil?
       session[:chosen_maps] << session[:chosen_map]
     end
-    if params[:reset] || session[:maps].empty?
+    if params[:reset]
       session[:chosen_map] = nil
       session[:chosen_maps] = []
       init_maps
