5a6888f0ace6ac6ac6fb074b90803dffdcc963c5
Murillo Parreira <murparreira@gmail.com>
2017-05-29 20:47:23 -0300

    Melhorar mensagens de erro ao salvar torneios. Adicionar numero máximo de mapas ao gerar rounds automaticamente.

--- app/assets/javascripts/tournaments.js
+++ app/assets/javascripts/tournaments.js
@@ -2,9 +2,11 @@ $(document).on('turbolinks:load', function() {
   $("#automatico").on("change", function() {
     if (this.checked) {
       $(".map_bans").show();
+      $(".numero_maximo_mapas").show();
     } else {
       $(".map_bans .nested-fields").remove()
       $(".map_bans").hide();
+      $(".numero_maximo_mapas").hide();
     }
   });
   $("#times_automatico").on("change", function() {
