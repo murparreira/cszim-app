7a7866e320d54ec7670306645e0ffd252103c348
Murillo Parreira <murillo.parreira@agrodefesa.go.gov.br>
2017-08-15 10:43:30 -0300

    Correções da conexão com o banco

--- app/controllers/randomizer_controller.rb
+++ app/controllers/randomizer_controller.rb
@@ -63,48 +63,50 @@ class RandomizerController < ApplicationController
   def finish
     chosen_map = RandomMap.last
     @torneio_dia = Tournament.find_by(nome: "Torneio #{Date.today.to_s(:human)}")
-    round = Round.create(tournament_id: @torneio_dia.id, map_id: chosen_map.map_id)
-    User.all.each do |u|
-      if u.steam.present?
-        @rankme_mysql = RankmeMysql.find_by(:steam => u.steam)
-        if @rankme_mysql.present?
-          @ct_team = Team.find_by(nome: "Time 1 CT #{Date.today.to_s(:human)}")
-          @tr_team = Team.find_by(nome: "Time 2 TR #{Date.today.to_s(:human)}")
-          if @rankme_mysql.ct_win.to_i > 0 || @rankme_mysql.tr_win.to_i > 0
-            rankme_attributes = @rankme_mysql.as_json.select{|k,v| k != 'id'}
-            if @rankme_mysql.ct_win.to_i > 0
-              rankme_attributes[:team_id] = @ct_team.id
-              Player.where(team_id: @ct_team.id, user_id: u.id).first_or_create
-            else
-              rankme_attributes[:team_id] = @tr_team.id
-              Player.where(team_id: @tr_team.id, user_id: u.id).first_or_create
-            end
-            rankme_attributes[:user_id] = u.id
-            rankme_attributes[:map_id] = chosen_map.id
-            rankme_attributes[:tournament_id] = @torneio_dia.id
-            rankme_attributes[:round_id] = round.id
-            @rankme_pg = Rankme.new(rankme_attributes)
-            @rankme_pg.save
-          end
-        end
-      end
-    end
     @rankme_mysql_ct = RankmeMysql.where('ct_win > 0').first
     @rankme_mysql_tr = RankmeMysql.where('tr_win > 0').first
-    if @rankme_mysql_ct && @rankme_mysql_tr
-      if @rankme_mysql_ct.ct_win == 7
+    if @rankme_mysql_ct.try(:ct_win) == 7 || @rankme_mysql_tr.try(:tr_win) == 7
+      round = Round.create(tournament_id: @torneio_dia.id, map_id: chosen_map.map_id)
+      if @rankme_mysql_ct.try(:ct_win) == 7
         Winner.create(team_id: @ct_team.id, round_id: round.id, placar: @rankme_mysql_ct.ct_win, lado: 'ct')
         Loser.create(team_id: @tr_team.id, round_id: round.id, placar: @rankme_mysql_tr.tr_win, lado: 't')
-      elsif @rankme_mysql_tr.tr_win == 7
+      elsif @rankme_mysql_tr.try(:tr_win) == 7
         Winner.create(team_id: @tr_team.id, round_id: round.id, placar: @rankme_mysql_tr.tr_win, lado: 't')
         Loser.create(team_id: @ct_team.id, round_id: round.id, placar: @rankme_mysql_ct.ct_win, lado: 'ct')
       end
+      User.all.each do |u|
+        if u.steam.present?
+          @rankme_mysql = RankmeMysql.find_by(:steam => u.steam)
+          if @rankme_mysql.present?
+            @ct_team = Team.find_by(nome: "Time 1 CT #{Date.today.to_s(:human)}")
+            @tr_team = Team.find_by(nome: "Time 2 TR #{Date.today.to_s(:human)}")
+            if @rankme_mysql.ct_win.to_i > 0 || @rankme_mysql.tr_win.to_i > 0
+              rankme_attributes = @rankme_mysql.as_json.select{|k,v| k != 'id'}
+              if @rankme_mysql.ct_win.to_i > 0
+                rankme_attributes[:team_id] = @ct_team.id
+                Player.where(team_id: @ct_team.id, user_id: u.id).first_or_create
+              else
+                rankme_attributes[:team_id] = @tr_team.id
+                Player.where(team_id: @tr_team.id, user_id: u.id).first_or_create
+              end
+              rankme_attributes[:user_id] = u.id
+              rankme_attributes[:map_id] = chosen_map.id
+              rankme_attributes[:tournament_id] = @torneio_dia.id
+              rankme_attributes[:round_id] = round.id
+              @rankme_pg = Rankme.new(rankme_attributes)
+              @rankme_pg.save
+            end
+          end
+        end
+      end
+      columns = RankmeMysql.columns.map(&:name)
+      columns_to_update = columns - ["id", "steam", "name", "lastip"]
+      columns_with_zero = columns_to_update.map { |c| [c, 0] }.to_h
+      RankmeMysql.update_all columns_with_zero
+      flash[:warning] = "Mapa finalizado com sucesso!"
+    else
+      flash[:warning] = "Mapa não teve um vencedor. Não foi gravado os dados!"
     end
-    columns = RankmeMysql.columns.map(&:name)
-    columns_to_update = columns - ["id", "steam", "name", "lastip"]
-    columns_with_zero = columns_to_update.map { |c| [c, 0] }.to_h
-    RankmeMysql.update_all columns_with_zero
-    flash[:warning] = "Mapa finalizado com sucesso!"
     redirect_to randomizer_url
   end
 
