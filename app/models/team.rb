dae87e33102121b1f31c51dbcc0e365afc8af75f
Murillo Parreira <murifox@gmail.com>
2017-08-15 22:22:02 -0300

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,16 +5,6 @@ class Team < ApplicationRecord
 
   validates :nome, presence: true, uniqueness: true
 
-  #before_validation :criar_nome_aleatorio
-
-  def criar_nome_aleatorio
-    player_names = ""
-    self.players.each do |player|
-      player_names += player.user.nome.first
-    end
-    self.nome = "#{rand(100)} #{player_names}"
-  end
-
   def self.safe_find_or_create_by(*args, &block)
     find_or_create_by *args, &block
   rescue ActiveRecord::RecordNotUnique
