2b1b8e8076dc1addc1a2dff5425a2db794950e67
Murillo Parreira <murparreira@gmail.com>
2023-02-06 18:56:15 -0300

    add: rota web do good_jobs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  mount GoodJob::Engine => 'good_job'
+
   resources :teams
   root to: 'dashboard#index'
 
@@ -19,13 +21,6 @@ Rails.application.routes.draw do
   get 'compare_players' => 'compare#compare_players'
   get 'get_maps_from_tournament' => 'compare#get_maps_from_tournament'
   get 'get_tournaments_from_season' => 'compare#get_tournaments_from_season'
-  get 'randomizer' => 'randomizer#index'
-  get 'randomizer_raffle' => 'randomizer#raffle'
-  get 'randomizer_reset' => 'randomizer#reset'
-  get 'randomizer_start' => 'randomizer#start'
-  get 'randomizer_finish' => 'randomizer#finish'
-  get 'randomizer_open_map' => 'randomizer#open_map'
-  get 'randomizer_select' => 'randomizer#select'
 
   get 'manager' => 'manager#index'
   get 'manager_raffle' => 'manager#raffle'
