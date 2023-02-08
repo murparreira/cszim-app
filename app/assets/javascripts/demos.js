36d7fd46a13a73213d5d8b4c63a7e08c8045eded
Murillo Parreira <murparreira@gmail.com>
2023-02-08 11:33:50 -0300

    update: melhora tela das demos

--- app/assets/javascripts/demos.js
+++ app/assets/javascripts/demos.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-  const tabs = document.querySelectorAll(".tabs li");
-  const tabContentBoxes = document.querySelectorAll("#tab-content > div");
+  const tabs = document.querySelectorAll("#round-tabs ul li");
+  const tabContentBoxes = document.querySelectorAll("#roundtabs-content > div.tab-content");
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
@@ -8,6 +8,23 @@ $(document).on('turbolinks:load', function() {
       tab.classList.add("is-active");
       const target = tab.dataset.target;
       tabContentBoxes.forEach(box => {
+        if (box.getAttribute("id") === target) {
+          box.classList.remove("is-hidden");
+        } else {
+          box.classList.add("is-hidden");
+        }
+      });
+    });
+  });
+  const mainTabs = document.querySelectorAll("#main-tabs ul li");
+  const mainTabContentBoxes = document.querySelectorAll("#maintabs-content > div.tab-content");
+
+  mainTabs.forEach((tab) => { 
+    tab.addEventListener("click", () => {
+      mainTabs.forEach(item => item.classList.remove("is-active"));
+      tab.classList.add("is-active");
+      const target = tab.dataset.target;
+      mainTabContentBoxes.forEach(box => {
         if (box.getAttribute("id") === target) {
           box.classList.remove("is-hidden");
         } else {
--- app/models/demo.rb
+++ app/models/demo.rb
@@ -4,4 +4,12 @@ class Demo < ApplicationRecord
   has_many :demo_rounds
   belongs_to :map
 
+  def ct_wins
+    demo_rounds.where(winningSide: 'CT').count
+  end
+
+  def t_wins
+    demo_rounds.where(winningSide: 'T').count
+  end
+
 end
