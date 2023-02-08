d08f23bb4bd4a6b3fa21e7f14433feb54104b2e3
Murillo Parreira <murparreira@gmail.com>
2023-02-08 12:25:09 -0300

    update: seeds removendo os rounds e kills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 ActiveRecord::Base.transaction do
 
   Game.destroy_all
@@ -19,6 +12,8 @@ ActiveRecord::Base.transaction do
   Round.destroy_all
   Team.destroy_all
   Demo.destroy_all
+  DemoRound.destroy_all
+  DemoRoundKill.destroy_all
   PlayerStatistic.destroy_all
   PlayerKill.destroy_all
 
